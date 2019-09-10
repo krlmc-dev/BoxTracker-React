d854d63ee032173e929791a66c49a5c54f84deb0
krlmc-dev <54382400+krlmc-dev@users.noreply.github.com>
2019-09-10 15:37:32 +1000

    clean up imports, add autofocus to button

--- src/Pages/QCRejectDialogue.jsx
+++ src/Pages/QCRejectDialogue.jsx
@@ -1,10 +1,10 @@
-import React from 'react';
 import Button from '@material-ui/core/Button';
 import Dialog from '@material-ui/core/Dialog';
 import DialogActions from '@material-ui/core/DialogActions';
 import DialogContent from '@material-ui/core/DialogContent';
 import DialogContentText from '@material-ui/core/DialogContentText';
 import DialogTitle from '@material-ui/core/DialogTitle';
+import React from 'react';
 
 export default function AlertDialog({onClick}) {
   const [open, setOpen] = React.useState(false);
@@ -19,7 +19,7 @@ export default function AlertDialog({onClick}) {
 
   return (
     <div>
-      <Button variant="contained" color="white" onClick={handleClickOpen}>
+      <Button variant="contained" color="white" autoFocus onClick={handleClickOpen}>
         Reject
       </Button>
       <Dialog
