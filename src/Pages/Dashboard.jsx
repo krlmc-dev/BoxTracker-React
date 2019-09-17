ddff7e3b57e94a69e50bca9ce49b146f36e9e10d
krlmc-dev <54382400+krlmc-dev@users.noreply.github.com>
2019-09-17 11:49:22 +1000

    Radial Chart to Bar chart, fix data inputs, add box table to page

--- src/Pages/Dashboard.jsx
+++ src/Pages/Dashboard.jsx
@@ -2,7 +2,8 @@
 import React, { PureComponent } from 'react';
 import ReactTable from 'react-table';
 import {
-  BarChart, Bar, Cell, XAxis, YAxis, CartesianGrid, Tooltip, Legend } from 'recharts';
+  BarChart, Bar, Cell, XAxis, YAxis, CartesianGrid, Tooltip, Legend
+} from 'recharts';
 //@material-react imports
 import Toolbar from "@material-ui/core/Toolbar";
 import { makeStyles } from '@material-ui/core/styles';
@@ -12,10 +13,12 @@ import Paper from '@material-ui/core/Paper';
 import Header from "./../Components/Headers/Header";
 import HeaderLinks from "./../Components/Headers/HeaderLinks";
 import ContinueDialogue from './ContinueDialogue';
+import ViewStats from './ViewStats';
 //css imports
 import "react-table/react-table.css";
 import '../customers.css';
 import '../Menu.css';
+import { minWidth } from '@material-ui/system';
 
 const useStyles = makeStyles(theme => ({
   root: {
@@ -42,7 +45,10 @@ export default class Dashboard extends React.Component {
       loading: false,
       go: false,
       show: false,
-      isShown: false
+      isShown: false,
+      perStep: [],
+      operatorList: [],
+      operatorCount: ""
     };
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
@@ -70,8 +76,9 @@ export default class Dashboard extends React.Component {
   }
 
   render() {
-    const { show, isShown, boxes, avg, stepNums, stepTimes, boxID } = this.state
-    const colors = []
+    const { show, isShown, boxes, avg, stepNums, stepTimes, perStep, operatorList, boxID } = this.state
+    const colors = ["#2b9900", "#006399", "#e59400", "#f00"]
+
     const style = {
       top: 0,
       left: 350,
@@ -107,57 +114,147 @@ export default class Dashboard extends React.Component {
           />
           <Toolbar />
           <header className="Menu-header">Dashboard</header>
-          <h2>Average Job Completion Time: 
+          <h2>Average Box Completion Time:
             {avg.map((data, i) => {
-              return (
-                <div>
-                  {data.average_job_length}
-                </div>
-              )
-            })
+            return (
+              <div>
+                {data.average_job_length}
+              </div>
+            )
+          })
             }
-            </h2>
-          <Paper className={useStyles.paper}>
-            <Grid container justify={"center"}>
-              <Grid>
-                <h2>Boxes At Each Workstation</h2>
-                <BarChart
-                  width={500}
-                  height={300}
-                  data={[stepNums[4], stepNums[5], stepNums[6], stepNums[7]]}
-                  margin={{
-                    top: 5, right: 30, left: 20, bottom: 5,
-                  }}
-                >
-                  <CartesianGrid strokeDasharray="3 3" />
-                  <XAxis dataKey="name" />
-                  <YAxis />
-                  <Tooltip />
-                  <Legend />
-                  <Bar dataKey="Number of Boxes">
-                    {
-                      
-                      colors[0] = "#2b9900",
-                      colors[1] = "#006399",
-                      colors[2] = "#e59400",
-                      colors[3] = "#f00",
-                      stepNums.map((entry, index) => (
-                        <Cell key={`cell-${index}`} fill={colors[index]}  strokeWidth={index === 2 ? 4 : 1}/>
-                      ))
+          </h2>
+          <Grid container justify={"center"}>
+            <Paper className={useStyles.paper}>
+              <Grid container justify={"center"}>
+                <Grid item>
+                  <h2>Boxes At Each Workstation</h2>
+                  <BarChart
+                    width={500}
+                    height={300}
+                    data={[stepNums[4], stepNums[5], stepNums[6], stepNums[7]]}
+                    margin={{
+                      top: 5, right: 30, left: 20, bottom: 5,
+                    }}
+                  >
+                    <CartesianGrid strokeDasharray="3 3" />
+                    <XAxis dataKey="name" />
+                    <YAxis />
+                    <Tooltip />
+                    <Legend />
+                    <Bar dataKey="Number of Boxes">
+                      {
+                        stepNums.map((entry, index) => (
+                          <Cell key={`cell-${index}`} fill={colors[index]} strokeWidth={index === 2 ? 4 : 1} />
+                        ))
+                      }
+                    </Bar>
+                  </BarChart>
+                </Grid>
+                <Grid item>
+                  <h2>Average Time Per Step</h2>
+                  <p>Preparation: {stepTimes.map((data, i) => { return (<div>{data.Preparation}</div>) })}</p>
+                  <p>Scanning: {stepTimes.map((data, i) => { return (<div>{data.Scan}</div>) })}</p>
+                  <p>Quality Control: {stepTimes.map((data, i) => { return (<div>{data.Quality_Control}</div>) })}</p>
+                  <p>Dispatch: {stepTimes.map((data, i) => { return (<div>{data.Dispatch}</div>) })}</p>
+                </Grid>
+                <Grid item>
+                  <h2>Boxes Completed Per Step Per Operator</h2>
+                  <BarChart
+                    width={500}
+                    height={300}
+                    data={[perStep[0],perStep[1],perStep[2], perStep[3]]}
+                    margin={{
+                      top: 5, right: 30, left: 20, bottom: 5,
+                    }}
+                  >
+                    <CartesianGrid strokeDasharray="3 3" />
+                    <XAxis dataKey="Name" />
+                    <YAxis />
+                    <Tooltip />
+                    <Legend />
+                    {operatorList.map((data, i) => { return (<Bar dataKey={data} fill={colors[i]} />) })}
+                    {//alert(perStep)
                     }
-                  </Bar>
-                </BarChart>
+
+                  </BarChart>
+                </Grid>
               </Grid>
-              <Grid item>
-                <h2>Average Time Per Step</h2>
-                <p>Preparation: {stepTimes.map((data, i) => {return (<div>{data.Preparation}</div>)})}</p>
-                <p>Scanning: {stepTimes.map((data, i) => {return (<div>{data.Scan}</div>)})}</p>
-                <p>Quality Control: {stepTimes.map((data, i) => {return (<div>{data.Quality_Control}</div>)})}</p>
-                <p>Dispatch: {stepTimes.map((data, i) => {return (<div>{data.Dispatch}</div>)})}</p>
-              </Grid>
+            </Paper>
+            <Grid item xs={9}>
+              <ReactTable 
+                getTrGroupProps={(state, rowInfo) => {
+                  if (rowInfo !== undefined) {
+                    return {
+                      onClick: () => {
+                        var path = "/dashboard/" + rowInfo.row.box_id
+                        this.props.history.push(path);
+                      },
+                      style: {
+                        cursor: 'pointer',
+                        background: rowInfo.original.id === this.state.selectedIndex ? '#00afec' : 'white',
+                        color: rowInfo.original.id === this.state.selectedIndex ? 'white' : 'black'
+                      }
+                    }
+                  }
+                }
+                }
+                data={boxes}
+                columns={[
+                  {
+                    Header: "Box",
+                    columns: [
+                      {
+                        Header: "Box ID",
+                        accessor: "box_id"
+                      },
+                      {
+                        Header: "Job ID",
+                        accessor: "job_id"
+                      },
+                      {
+                        Header: "Customer",
+                        accessor: "customer_name"
+                      },
+                      {
+                        Header: "Location",
+                        accessor: "box_location"
+                      },
+                      {
+                        Header: "Operator",
+                        accessor: "box_operator"
+                      },
+                      {
+                        Header: "Step",
+                        accessor: "box_step"
+                      },
+                      {
+                        Header: "State",
+                        accessor: "box_state"
+                      },
+                      {
+                        Header: "Dispatch",
+                        accessor: "job_dispatch"
+                      }
+                    ]
+                  }
+                ]}
+                filterable
+                defaultPageSize={10}
+                className="-striped -highlight"
+              >
+                {(state, makeTable, instance) => {
+                  return (
+
+                    <div>
+                      {makeTable()}
+                    </div>
+                  )
+                }}
+              </ReactTable>
             </Grid>
-          </Paper>
-          
+          </Grid>
+
         </div>
       </div>
     );
@@ -207,7 +304,10 @@ export default class Dashboard extends React.Component {
           this.setState({
             avg: response,
             stepNums: response[1],
-            stepTimes: response[2]
+            stepTimes: response[2],
+            perStep: response[3],
+            operatorList: response[3][4],
+            operatorCount: response[3][5]
           })
         }
         return response;
